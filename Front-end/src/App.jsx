d6defa7fd1ed3c598fa0ac344a2130194f4872a7
rianpriansyah <rian.priansyah04@gmail.com>
2025-11-03 17:32:20 +0700

    feat(front-end): add forgotPassword layout

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,14 @@
 // import Enter from "./pages/Enter";
 // import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     // <Enter />
     // <Login />
-    <Register />
+    // <Register />
+    <ForgotPassword />
   );
 }
 
---
+++ Front-end/src/pages/ForgotPassword.jsx
@@ -0,0 +1,42 @@
+import forgotPasswordIcon from "../assets/images/Forgot-password-cuate.svg";
+import envelopeIcon from "../assets/images/envelope-solid-full.svg";
+
+export default function ForgotPassword() {
+  return (
+    <div className="bg-[#1E1633] font-ubuntu h-screen w-full flex justify-center items-center">
+      <div className="w-1/4">
+        <h1 className="text-center text-[#ffec78] text-[1.8rem] font-bold mb-2">
+          Forgot
+        </h1>
+        <img src={forgotPasswordIcon} alt="" className="w-3/5 mx-auto" />
+        <p className="text-center text-[#ffec78] text-[1.5rem] font-semibold mb-2">
+          Forgot Password?
+        </p>
+        <p className="text-center text-white leading-6.5 mb-8">
+          Don’t worry! It happens. Please enter the email address with your
+          account
+        </p>
+        <form>
+          <label
+            htmlFor="email"
+            className="text-white font-bold text-[12px] ml-2.5"
+          >
+            E-mail Address
+          </label>
+          <div className="flex items-center gap-x-4 bg-[#ffec78] py-3 px-5 mt-2 mb-1.5 rounded-xl">
+            <img src={envelopeIcon} alt="" className="w-7" />
+            <input
+              type="email"
+              className="w-full outline-0"
+              placeholder="johndoe@gmail.com"
+            />
+          </div>
+
+          <button className="w-full bg-transparent border border-[#ffec78] text-[#ffec78] text-[25px] font-bold py-2.5 mt-[25px] rounded-xl cursor-pointer transition duration-300 ease-in-out hover:border-transparent hover:bg-[#ffec78] hover:text-[#1E1633]">
+            Get OTP
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
