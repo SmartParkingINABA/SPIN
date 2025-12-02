5f77179e9ecf3ebce9052836b4f379ace8a6aff7
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-02 22:56:04 +0700

    refactor: logic func send OTP, send notif password changes

--- src/utils/SendMail.js
+++ src/utils/SendMail.js
@@ -19,6 +19,25 @@ const sendMail = async (to, subject, html) => {
             html
         }
     );
+
 };
 
-export default sendMail;
+const sendOtpToEmail = (email, otp) => {
+    const html = `
+    <p>Kode OTP Anda:</p>
+    <h2>${otp}</h2>
+    <p>Jangan Berikan Kode Ini Kepada Siapapun</p>`;
+    return sendMail(email, 'Kode OTP Verifikasi', html);
+};
+
+const sendNotifPasswordChangedEmail = (email) => {
+    const html = `
+    <p>Halo, </p>
+    <p>Password Akun Anda Telah Berhasil Diubah</p>
+    <p>Jika Ini Bukan Anda, Segera Lakukan Reset Password</p>
+    <br>
+    <p>Salam hormat, </p>
+    <p><b>SPIN Parking Team</b></p>`;
+    return sendMail(email, 'Password Anda Berhasil Diubah', html);
+}
+export { sendMail, sendOtpToEmail, sendNotifPasswordChangedEmail };
