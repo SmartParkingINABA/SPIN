72fc2e6caf70a6386829a0197b3ac98c7073fb7c
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-02 23:23:03 +0700

    logic petugas status

---
+++ src/middlewares/PetugasStatusMiddlewares.js
@@ -0,0 +1,36 @@
+import redis from "../configs/RedisConfig.js";
+
+const statusPetugas = async (req, res, next) => {
+    try {
+        const user = req.user;
+
+        if (!user || user.role.nama_role !== 'petugas') {
+            return res.status(403).json(
+                {
+                    message: 'Akses Di Tolak!, Hanya Petugas Yang Di Izinkan'
+                }
+            );
+        }
+        const key = `petugas:presence:${user.id_users}`;
+        const status = await redis.get(key);
+
+        if(!status) {
+            return res.status(401).json(
+                {
+                    message: 'Petugas Tidak Aktif'
+                }
+            );
+        }
+        req.status = JSON.parse(status);
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(
+            {
+                message: 'Internal Server Error!'
+            }
+        );
+    }
+};
+
+export default statusPetugas;
