884448294b5dccc6d7911ec87331c9a7d44b9c47
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-05 16:42:46 +0700

    chore: update logout with set cookies

--- src/controllers/LogOutControllers.js
+++ src/controllers/LogOutControllers.js
@@ -1,20 +1,41 @@
-import redis from "../configs/RedisConfig.js";
+import { deleteSession } from "../utils/SessionService.js";
+
+const cookieName = 'sessionId';
 
 const logoutController = {
     async handleLogout(req, res) {
         try {
-            const userId = req.user.id;
-            const sessionId = req.headers['x-session-id'];
-
-            if (!sessionId) {
+            const cookie = req.cookies?.sessionId;
+            if (!cookie) {
                 return res.status(400).json(
                     {
-                        message: 'Session Id Tidak Ditemukan!'
+                        message: 'Session Cookie Tidak Ditemukan!'
                     }
                 );
             };
-            const sessionKey = `session:${userId}:${sessionId}`;
-            await redis.del(sessionKey);
+            const [userIdStr, sessionId]= cookie.split(':');
+            const userId = parseInt(userIdStr, 10);
+
+            if (!userId || !sessionId) {
+                res.clearCookie(cookieName, {
+                    httpOnly: true,
+                    samesite: 'strict',
+                    secure: process.env.NODE_ENV === 'production'
+                });
+                return res.status(400).json(
+                    {
+                        message: 'Cookie Invalid, sudah dihapus'
+                    }
+                );
+            };
+
+            await deleteSession(userId, sessionId);
+            res.clearCookie(cookieName, {
+                httpOnly: true,
+                samesite: 'strict',
+                secure: process.env.NODE_ENV === 'production'
+            });
+
             return res.status(200).json(
                 {
                     message: 'Logout Berhasil'
@@ -25,7 +46,7 @@ const logoutController = {
             console.error(err);
             return res.status(500).json(
                 {
-                    message: 'Internal Server Error'
+                    message: 'Internal Server Error!'
                 }
             );
         }
