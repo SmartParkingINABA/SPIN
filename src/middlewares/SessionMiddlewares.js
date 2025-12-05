ff7bb7b0dcac64005e48c247c072a935047bfd81
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-05 16:55:46 +0700

    chore: update cookies

--- src/middlewares/SessionMiddlewares.js
+++ src/middlewares/SessionMiddlewares.js
@@ -2,17 +2,26 @@ import { sessionValid } from "../utils/SessionService.js";
 
 const verifySession = async (req, res, next) => {
     try {
-        const user = req.user;
-        const sessionId = req.headers['x-session-id'] || req.headers['X-Session-Id'];
+        const cookie = req.cookies?.sessionId;
 
-        if (!sessionId) {
+        if (!cookie) {
             return res.status(401).json(
                 {
-                    message: 'Session Id Tidak Ditemukan!'
+                    message: 'Session Cookie Tidak Ditemukan!'
                 }
             );
         };
 
+        const [userIdStr, sessionId] = cookie.split(':');
+        const userId = parseInt(userIdStr, 10);
+
+        if (!userId || !sessionId) {
+            return res.status(401).json(
+                {
+                    message: 'Session Cookie Tidak Valid!'
+                }
+            )
+        };
         const valid = await sessionValid(user.id_users, sessionId);
         if (!valid) {
             return res.status(401).json(
@@ -21,12 +30,14 @@ const verifySession = async (req, res, next) => {
                 }
             );
         };
+
+        req.session = { userId, sessionId };
         next();
     } catch(err) {
         console.error('Session middleware error: ', err);
         return res.status(500).json(
             {
-                message: 'Internal Server Error'
+                message: 'Internal Server Error!'
             }
         );
     };
