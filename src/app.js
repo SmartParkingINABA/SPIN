a800e85433995dbc624dcf62f038d6d1c89da13e
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-05 16:28:40 +0700

    fix: cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import dotenv from 'dotenv'
 dotenv.configDotenv({path: './src/environments/.env'});
 import cors from 'cors'
 import express from 'express'
+import cookieParser from 'cookie-parser';
 import indexRoutes from './routes/IndexRoutes.js';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: process.env.CLIENT_SIDE_URL,
+    credentials: true,
+}));
+
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use('/api', indexRoutes);
