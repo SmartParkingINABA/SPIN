d5809ee0a937cc11abf372cb0d130adb75809d34
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-05 16:53:17 +0700

    fix: identify role petugas

--- src/middlewares/PetugasStatusMiddlewares.js
+++ src/middlewares/PetugasStatusMiddlewares.js
@@ -4,7 +4,7 @@ const statusPetugas = async (req, res, next) => {
     try {
         const user = req.user;
 
-        if (!user || user.role.nama_role !== 'petugas') {
+        if (!user || user.role.nama_role.toLowerCase() !== 'petugas') {
             return res.status(403).json(
                 {
                     message: 'Akses Di Tolak!, Hanya Petugas Yang Di Izinkan'
