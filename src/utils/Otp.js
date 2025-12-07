0bc5ceed398769800eaf379ca7b16bba9cdfc1c0
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-07 11:57:19 +0700

    chore: update name file Otp

---
+++ src/utils/Otp.js
@@ -0,0 +1,21 @@
+import redis from "../configs/RedisConfig.js";
+
+const generateOtp = async (email) => {
+    const otp = Math.floor(1000 + Math.random() * 9000).toString();
+    await redis.set(`otp:${otp}`, email, 'EX', 300);
+    return otp;
+
+};
+
+const verifyOtp = async (otpInput) => {
+    const email = await redis.get(`otp:${otpInput}`);
+    return email;
+
+};
+
+const deleteOtp = async (otpInput) => {
+    await redis.del(`otp:${otpInput}`);
+    
+};
+
+export { generateOtp, verifyOtp, deleteOtp };
