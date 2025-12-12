f476622bd249722cd6a88bdb7974d7354279c71d
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-12 21:08:11 +0700

    testing fungsionalitas ttl

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -12,7 +12,7 @@ const cookieOptions = (req) => (
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
-        maxAge: 1000 * 60 * 60 * 6,
+        maxAge:  1000 * 60 * 60 * 6,
     }
 );
 
