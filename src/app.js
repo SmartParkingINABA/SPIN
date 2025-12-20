13c65cc91edfe08629af4f5a5090994bb65defc4
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-20 21:16:46 +0700

    refactor: server express and socket

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.configDotenv({path: './src/environments/.env'});
 import cors from 'cors'
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import indexRoutes from './routes/IndexRoutes.js';
 
 const app = express();
+
+app.set('trust proxy', 1);
 
 app.use(cors({
     origin: process.env.CLIENT_SIDE_URL,
@@ -18,6 +18,14 @@ app.use(cookieParser());
 
 app.use('/api', indexRoutes);
 
+app.use((err, req, res, next) => {
+    console.error('Global Erro: ', err);
+    res.status(err.status || 500).json(
+        {
+            message: 'Internal Server Error!'
+        }
+    )
+})
 
 export default app;
 
