2ab7a9197d4522b4c8fd088b598e8112318c3b16
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-24 19:15:09 +0700

    refactor: deleted endpoint admin, petugas, pengendara in auth routes

--- src/routes/authentication/AuthRoutes.js
+++ src/routes/authentication/AuthRoutes.js
@@ -29,33 +29,5 @@ authRoutes.get(
     logoutController.handleLogout
 )
 
-//membatasi hak akses halaman berdasarkan role-user (admin, petugas, pengendara)
-authRoutes.get(
-    '/admin/dashboard',
-    authenticationRoleBasedUser(['admin']),
-    verifySession,
-    dashboardAdminControllers.getOverview
-);
-
-authRoutes.get(
-    '/petugas/dashboard',
-    authenticationRoleBasedUser(['petugas']),
-    verifySession,
-    (req, res) => {
-        res.status(200).json({
-            message: `Selamat Datang Di Dashboard Petugas, ${req.user.petugasProfile?.nama_petugas || req.user.email}`,
-        });
-    }
-);
-
-authRoutes.get(
-    '/pengendara/dashboard',
-    authenticationRoleBasedUser(['pengendara']),
-    (req, res) => {
-        res.status(200).json({
-            message: `Selamat Datang Di Dashboard Pengendara, ${req.user.pengendaraProfile?.nama_pengendara || req.user.email}`,
-        })
-    }
-)
 
 export default authRoutes;
