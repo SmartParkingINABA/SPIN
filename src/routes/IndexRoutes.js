7a27b58d0c6cc22f4063f76c5ebb1a95bbc2043a
Nico Ramdani <nicoramdani3@gmail.com>
2025-12-25 22:10:47 +0700

    chore: update new route and add routes enpoint dashboard petugas

--- src/routes/IndexRoutes.js
+++ src/routes/IndexRoutes.js
@@ -3,6 +3,7 @@ import authRoutes from './authentication/AuthRoutes.js'
 import forgotPasswordRoutes from './authentication/ForgotPasswordRoutes.js';
 import adminRoutes from './admin/AdminRoutes.js';
 import dashboardPengendara from './pengendara/pengendaraRoutes.js';
+import dashboardPetugas from './petugas/petugasRoutes.js';
 
 const indexRoutes = express.Router();
 
@@ -10,6 +11,7 @@ const indexRoutes = express.Router();
 indexRoutes.use('/auth', authRoutes);
 indexRoutes.use('/auth/forgot', forgotPasswordRoutes);
 indexRoutes.use('/admin', adminRoutes);
+indexRoutes.use('/petugas', dashboardPetugas)
 indexRoutes.use('/pengendara', dashboardPengendara);
 
 
