c68d6392edfd23c3482f9e18f90de04795a1a165
Nico Ramdani <nicoramdani3@gmail.com>
2026-01-19 22:32:57 +0700

    chore: update socket room admin

--- src/socket/admin/adminSocket.js
+++ src/socket/admin/adminSocket.js
@@ -1,14 +1,40 @@
 import statusCache from "../../cache/Status.js";
+import menuNotifikasi from "../../services/admin/menuNotifikasi.js";
 
 const adminSocket = (io) => {
     io.on('connection', (socket) => {
-        socket.join('admin');
+
+        if (socket.user?.role === 'Admin') {
+            socket.join('Admin');
+        }
+
+        if (socket.user?.role === 'Petugas') {
+            socket.join(`petugas:${socket.user.petugasProfile.id_petugas}`)
+            socket.join('role:petugas')
+        }
+
+        if (socket.user?.role === 'Pengendara') {
+            socket.join(`pengendara:${socket.user.pengendaraProfile.id_pengendara}`)
+            socket.join('role:pengendara')
+        }
 
         socket.on('dashboard:request', async () => {
             const total = await statusCache.totalPetugasOnline();
             socket.emit('petugas:status:total', total)
         });
+        
+        socket.on('dashboard:init', async () => {
+            try {
+                const stats = await menuNotifikasi.getDashboardStats();
+                socket.emit('dashboard:stats', stats);
+            } catch (error) {
+                socket.emit('dashboard:error', {
+                    message: 'Gagal memuat data statistik'
+                })
+            }
+        })
     });
+
 };
 
 export default adminSocket;
