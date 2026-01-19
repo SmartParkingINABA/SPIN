34eace5135c664ef83ec8eb37d3d379333748154
Nico Ramdani <nicoramdani3@gmail.com>
2026-01-19 22:35:30 +0700

    chore: update and add new emit notification

--- src/socket/emitter/notifikasiEmitter.js
+++ src/socket/emitter/notifikasiEmitter.js
@@ -16,7 +16,43 @@ const emitNotifikasiPengendara = (pengendaraId, payload) => {
     
 };
 
+const emitNotifToPengendara = (payload) => {
+    if (!ioInstance) return;
+
+    ioInstance
+    .to('role:pengendara')
+    .emit('notifikasi:new', payload);
+    console.log('Notifikasi Baru: ');
+    console.log(JSON.stringify(payload, null, 2));
+    
+    
+};
+
+const emitNotifToPetugas = (payload) => {
+    if (!ioInstance) return;
+
+        ioInstance
+        .to('role:petugas')
+        .emit('notifikasi:new', payload)
+        console.log('Notifikasi Baru: ');
+        console.log(JSON.stringify(payload, null, 2));
+        
+    
+};
+
+const emitNotifToAllUser = (payload) => {
+    if (!ioInstance) return;
+
+    ioInstance
+    .emit('notifikasi:new', payload);
+    console.log('Notifikasi Baru: ');
+    console.log(JSON.stringify(payload, null, 2));
+}
+
 export {
     setSocketIo,
-    emitNotifikasiPengendara
+    emitNotifikasiPengendara,
+    emitNotifToPengendara,
+    emitNotifToPetugas,
+    emitNotifToAllUser
 }
