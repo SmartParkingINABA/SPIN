d7110220d1e8180d9298be04339958faeaae53d8
Rian Priansyah <rian.priansyah04@gmail.com>
2026-02-17 11:29:06 +0700

    chore(front-end): update file name env and modify

--- Front-end/src/lib/axios.js
+++ Front-end/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 if (!baseURL) {
   throw new Error("BaseURL is not defined");
