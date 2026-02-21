f9ef358df955e0caea70114af33b43fba71ec486
Rian Priansyah <rian.priansyah04@gmail.com>
2026-02-21 11:52:26 +0700

    fix: update chunk Loader

--- Front-end/src/components/Loader.jsx
+++ Front-end/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white/70 backdrop-blur-sm z-50">
-      <p className="font-bold text-2xl animate-pulse">Loading...</p>
+      <p className="font-bold text-2xl animate-pulse">Loading...😁😁</p>
     </div>
   );
 }
