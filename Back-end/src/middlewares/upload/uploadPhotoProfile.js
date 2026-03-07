04875f93e72a749e7185268269e55c09644a0f50
Nico Ramdani <nicoramdani3@gmail.com>
2026-03-07 07:13:06 +0700

    update middlewares uploaded pp

--- Back-end/src/middlewares/upload/uploadPhotoProfile.js
+++ Back-end/src/middlewares/upload/uploadPhotoProfile.js
@@ -1,31 +1,19 @@
 import multer from "multer";
-import path from 'path';
-import fs from 'fs';
 
-const uploadDir = 'public/uploads/profile';
-
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, {recursive: true});
-};
-
-const storage = multer.diskStorage({
-    destination: (_, __, cb) => cb(null, uploadDir),
-    filename: (req, file, cb) => {
-        const text = path.extname(file.originalname);
-        cb(null, `profile-${req.user.id_users}-${Date.now()}${text}`);
-    }
-});
-
+const storage = multer.memoryStorage();
+const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg"];
 const fileFilter = (req, file, cb) => {
-    const allowed = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (!allowed.includes(file.mimetype)) {
-        return cb(new Error('File harus berformat JPG/PNG'));
+    if (!allowedMimeTypes.includes(file.mimetype)) {
+        return cb(new Error("File harus berformat JPEG, JPG, dan PNG!"), false);
     }
+
     cb(null, true);
 };
 
-export default multer({
+const uploadFotoProfile = multer({
     storage,
     fileFilter,
     limits: { fileSize: 2 * 1024 * 1024 }
 });
+
+export default uploadFotoProfile;
