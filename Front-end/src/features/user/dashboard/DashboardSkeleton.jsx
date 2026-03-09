11ad08d434bf48fdfc699d3a0772d0350f507a72
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-09 15:24:30 +0700

    update dashboardskeleton ui

--- Front-end/src/features/user/dashboard/DashboardSkeleton.jsx
+++ Front-end/src/features/user/dashboard/DashboardSkeleton.jsx
@@ -16,7 +16,7 @@ export default function DashboardSkeleton() {
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
           <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
-          <div className="flex flex-col gap-x-2.5 mt-3.5">
+          <div className="flex flex-col gap-2 mt-3.5">
             {[...Array(2)].map((_, i) => (
               <div className="h-10 rounded-md bg-[#130F40]" key={i}></div>
             ))}
@@ -26,7 +26,7 @@ export default function DashboardSkeleton() {
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
           <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
-          <div className="flex flex-col gap-2.5 mt-3.5">
+          <div className="flex flex-col gap-2 mt-3.5">
             {[...Array(2)].map((_, i) => (
               <div className="h-10 rounded-md bg-[#130F40]" key={i}></div>
             ))}
@@ -36,7 +36,7 @@ export default function DashboardSkeleton() {
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
           <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
-          <div className="flex flex-col gap-2.5 mt-3.5">
+          <div className="flex flex-col gap-2 mt-3.5">
             {[...Array(2)].map((_, i) => (
               <div className="h-10 rounded-md bg-[#130F40]" key={i}></div>
             ))}
