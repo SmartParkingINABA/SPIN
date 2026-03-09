ae377daf23c5c4fc51b6076f2eaa4c0a777cbfbc
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-09 15:13:27 +0700

    update dahsboardskeleton ui

--- Front-end/src/features/user/dashboard/DashboardSkeleton.jsx
+++ Front-end/src/features/user/dashboard/DashboardSkeleton.jsx
@@ -1,13 +1,13 @@
 export default function DashboardSkeleton() {
   return (
     <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <h1 className="h-4 bg-[#93A3B6] rounded-md"></h1>
-      <p className="h-3 bg-[#93A3B6] rounded-md mt-1"></p>
+      <h1 className="h-4 w-[20%] bg-[#93A3B6] rounded-md"></h1>
+      <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-6">
         <div className="grid grid-cols-4 gap-x-6">
           {[...Array(4)].map((_, i) => (
             <div
-              className="h-16 border border-[rgba(255,236,120,0.5)] bg-[#1E1633] rounded-md"
+              className="h-20 border border-[rgba(255,236,120,0.5)] bg-[#1E1633] rounded-md"
               key={i}
             ></div>
           ))}
@@ -15,25 +15,31 @@ export default function DashboardSkeleton() {
       </div>
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
-          <p className="h-3 w-[20%] bg-[#93A3B6] rounded-md"></p>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
           {[...Array(2)].map((_, i) => (
-            <div className="mt-3.5 h-10 rounded-md bg-[#130F40]" key={i}></div>
+            <div className="flex flex-col gap-2.5" key={i}>
+              <div className="h-10 rounded-md bg-[#130F40]"></div>
+            </div>
           ))}
         </div>
       </div>
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
-          <p className="h-3 w-[20%] bg-[#93A3B6] rounded-md"></p>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
           {[...Array(2)].map((_, i) => (
-            <div className="mt-3.5 h-10 rounded-md bg-[#130F40]" key={i}></div>
+            <div className="flex flex-col gap-2.5" key={i}>
+              <div className="h-10 rounded-md bg-[#130F40]"></div>
+            </div>
           ))}
         </div>
       </div>
       <div className="mt-6">
         <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
-          <p className="h-3 w-[20%] bg-[#93A3B6] rounded-md"></p>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md"></p>
           {[...Array(2)].map((_, i) => (
-            <div className="mt-3.5 h-10 rounded-md bg-[#130F40]" key={i}></div>
+            <div className="flex flex-col gap-2.5" key={i}>
+              <div className="h-10 rounded-md bg-[#130F40]"></div>
+            </div>
           ))}
         </div>
       </div>
