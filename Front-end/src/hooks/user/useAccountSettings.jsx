b6842d34f8a20f7a47f2066801f865c864987d0b
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-12 18:46:00 +0700

    test user profile api 5.5

--- Front-end/src/hooks/user/useAccountSettings.jsx
+++ Front-end/src/hooks/user/useAccountSettings.jsx
@@ -8,7 +8,7 @@ import {
 
 export const useGetAccountSettings = () => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const handleDataProfile = async () => {
