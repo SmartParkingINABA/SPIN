68540e4decda05ea026d0249dd18b9becbfce3db
Nico Ramdani <nicoramdani3@gmail.com>
2026-03-14 02:06:01 +0700

    refactor: using method transaction for update profile picture between db and bucket

--- Back-end/src/controllers/upload/fotoProfile.js
+++ Back-end/src/controllers/upload/fotoProfile.js
@@ -3,6 +3,8 @@ import generateProfileFilename from "../../utils/generateFilename.js";
 import sequelize from "../../configs/DBConfig.js";
 
 export const updateProfilePhoto = async (req, res) => {
+    const transaction = await sequelize.transaction();
+
     try {
         if (!req.file) {
             return res.status(400).json({
@@ -11,6 +13,19 @@ export const updateProfilePhoto = async (req, res) => {
         }
 
         const userId = req.user.id_users;
+
+        const profile = await sequelize.models.pengendaraProfile.findOne({
+            where: { user_id: userId },
+            transaction
+        });
+
+        if (!profile) {
+            await transaction.rollback();
+            return res.status(404).json({
+                message: 'Profil pengendara tidak ditemukan'
+            });
+        }
+
         const key = generateProfileFilename(userId, req.file.originalname);
 
         const imageUrl = await uploadPictureToS3({
@@ -19,30 +34,30 @@ export const updateProfilePhoto = async (req, res) => {
             mimetype: req.file.mimetype
         });
 
-        const user = await sequelize.models.Users.findByPk(userId);
+        const oldImageKey = profile.picture_private_key;
 
-        if (user?.picture_private_key) {
-            await deletePictureFromS3(user.picture_private_key);
-        }
-
-        await sequelize.models.Users.update(
+        await profile.update(
             {
                 foto_profil: imageUrl,
                 picture_private_key: key
             },
-            {
-                where: { id_users: userId }
-            }
+            { transaction }
         );
+        await transaction.commit();
+
+        if (oldImageKey) {
+            await deletePictureFromS3(oldImageKey);
+        }
 
         return res.json({
             message: 'Foto profil berhasil diperbarui',
-            data : {
-                foto_profil: imageUrl,
+            data: {
+                foto_profil: imageUrl
             }
-        });
+        })
 
     } catch (error) {
+        await transaction.rollback();
         console.error(error);
         return res.status(500).json({
             message: 'Internal server error'
