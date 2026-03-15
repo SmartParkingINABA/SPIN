c2b9ef5fded9e12fb1442436952770096e87910f
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-15 15:03:05 +0700

    add logout function in profile account

--- Front-end/src/features/user/profile/components/ButtonCta.jsx
+++ Front-end/src/features/user/profile/components/ButtonCta.jsx
@@ -1,11 +1,24 @@
 import { LuArrowRightFromLine } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/useAuth";
 
 export default function ButtonCta() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/auth/login", { replace: true });
+  };
+
   return (
     <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md w-1/3">
       <p className="text-[#FEF8FD] font-medium">Aksi</p>
       <div className="mt-3.5">
-        <button className="w-full flex items-center gap-x-3 bg-red-500 rounded-sm py-1.5 px-3 cursor-pointer opacity-100 hover:opacity-80 transition">
+        <button
+          className="w-full flex items-center gap-x-3 bg-red-500 rounded-sm py-1.5 px-3 cursor-pointer opacity-100 hover:opacity-80 transition"
+          onClick={handleLogout}
+        >
           <LuArrowRightFromLine className="w-5 h-fit text-[#FEF8FD]" />
           <p className="text-[#FEF8FD]">Logout</p>
         </button>
