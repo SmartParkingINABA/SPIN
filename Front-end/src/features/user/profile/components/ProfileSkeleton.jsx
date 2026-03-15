db6ec43230862498090a6f7430d160361a133d98
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-15 11:33:35 +0700

    update user profileskeleton ui 3.5

--- Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
+++ Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
@@ -9,23 +9,23 @@ export default function ProfileSkeleton() {
             <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md w-2/3">
               <div className="flex items-center justify-between">
                 <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md mt-2.5"></p>
-                <button className="bg-[#130F40] border border-[#93A3B6] rounded-sm"></button>
+                <p className="h-3 w-[10%] bg-[#130F40] border border-[#93A3B6] rounded-sm"></p>
               </div>
               <form className="mt-3.5 w-1/2" noValidate>
                 <div className="mb-2.5">
-                  <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mt-1"></p>
                 </div>
                 <div className="mb-2.5">
-                  <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mt-1"></p>
                 </div>
                 <div className="mb-2.5">
-                  <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mt-1"></p>
                 </div>
                 <div className="">
-                  <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mt-1"></p>
                 </div>
               </form>
@@ -34,8 +34,8 @@ export default function ProfileSkeleton() {
               <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
                 <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md mb-6"></p>
                 <div className="w-30 h-30 mx-auto rounded-full bg-[#93A3B6]"></div>
-                <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md text-center mt-4"></p>
-                <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md text-center"></p>
+                <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mx-auto mt-4 mb-2"></p>
+                <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md mx-auto"></p>
                 <p className="bg-[#93A3B6] rounded-sm py-1.5  w-full mt-3.5"></p>
               </div>
               <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
@@ -43,15 +43,15 @@ export default function ProfileSkeleton() {
                 <div className="mt-3.5 flex flex-col gap-y-3.5">
                   <div className="pb-3 border-b border-b-[rgba(255,236,120,0.5)]">
                     <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
-                    <p className="h-3.5 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3.5 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                   <div className="pb-3 border-b border-b-[rgba(255,236,120,0.5)]">
                     <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
-                    <p className="h-3.5 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3.5 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                   <div className="">
                     <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md"></p>
-                    <p className="h-3.5 w-[13%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3.5 w-[15%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                 </div>
               </div>
