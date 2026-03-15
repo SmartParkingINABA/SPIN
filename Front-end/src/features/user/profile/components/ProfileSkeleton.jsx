4fa7181a0f7e6511fd72d11d8c1abfdae57e22a1
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-15 12:27:46 +0700

    update user profileskeleton ui 9.0

--- Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
+++ Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
@@ -36,7 +36,7 @@ export default function ProfileSkeleton() {
                 <div className="w-30 h-30 mx-auto rounded-full bg-[#93A3B6]"></div>
                 <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mx-auto mt-4 mb-2"></p>
                 <p className="h-3 w-[18%] bg-[#130F40] rounded-md mx-auto"></p>
-                <p className="bg-[#93A3B6] rounded-sm py-1.5 h-4 w-full mt-3.5"></p>
+                <p className="bg-[#93A3B6] rounded-sm py-1.5 h-5 w-full mt-3.5"></p>
               </div>
               <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-6 py-4 rounded-md">
                 <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
