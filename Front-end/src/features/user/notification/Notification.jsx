a30e14ec36155f0e64e098ffd252d433e77c93ec
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-20 12:20:32 +0700

    add user notificationSkeleton ui

--- Front-end/src/features/user/notification/Notification.jsx
+++ Front-end/src/features/user/notification/Notification.jsx
@@ -2,29 +2,33 @@ import Header from "./components/Header";
 // import { notifications } from "./components/notificationsDummy";
 import NotificationCard from "./components/NotificationCard";
 import { useNotifications } from "../../../hooks/user/useNotifications";
+import NotificationSkeleton from "./components/NotificationSkeleton";
 
 export default function Notification() {
   const { notifications, loading, markRead, markAllRead } = useNotifications();
 
-  if (loading)
-    return <div className="text-black p-5">Memuat notifikasi...</div>;
-
   return (
-    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <Header notifications={notifications} onMarkAllRead={markAllRead} />
-      <div className="mt-6 flex flex-col gap-y-3.5">
-        {notifications.length > 0 ? (
-          notifications.map((item) => (
-            <NotificationCard
-              key={item.id}
-              data={item}
-              onMarkRead={() => markRead(item.id)}
-            />
-          ))
-        ) : (
-          <p className="text-[#93A3B6]">Belum ada notifikasi.</p>
-        )}
-      </div>
-    </section>
+    <>
+      {loading ? (
+        <NotificationSkeleton />
+      ) : (
+        <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+          <Header notifications={notifications} onMarkAllRead={markAllRead} />
+          <div className="mt-6 flex flex-col gap-y-3.5">
+            {notifications.length > 0 ? (
+              notifications.map((item) => (
+                <NotificationCard
+                  key={item.id}
+                  data={item}
+                  onMarkRead={() => markRead(item.id)}
+                />
+              ))
+            ) : (
+              <p className="text-[#93A3B6]">Belum ada notifikasi.</p>
+            )}
+          </div>
+        </section>
+      )}
+    </>
   );
 }
