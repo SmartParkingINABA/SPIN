95875e5037349886af59cabb451cff6288ff6b6c
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-20 14:31:51 +0700

    update parking history header

--- Front-end/src/features/user/parking-history/components/Header.jsx
+++ Front-end/src/features/user/parking-history/components/Header.jsx
@@ -1,10 +1,15 @@
+import ExportData from "./ExportData";
+
 export default function Header() {
   return (
-    <>
-      <h1 className="text-[#FEF8FD] text-2xl font-bold">Riwayat Parkir</h1>
-      <p className="text-[#93A3B6] font-medium mt-1">
-        Catatan aktivitas parkir kendaraan anda
-      </p>
-    </>
+    <div className="flex items-center justify-between">
+      <div>
+        <h1 className="text-[#FEF8FD] text-2xl font-bold">Riwayat Parkir</h1>
+        <p className="text-[#93A3B6] font-medium mt-1">
+          Catatan aktivitas parkir kendaraan anda
+        </p>
+      </div>
+      <ExportData />
+    </div>
   );
 }
