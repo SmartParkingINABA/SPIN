c1ce095da4a8f4993d48ea01fe60b1dd3c7b9e45
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-25 16:09:07 +0700

    add: parkingHistorySkeleton ui

--- Front-end/src/features/user/parking-history/ParkingHistory.jsx
+++ Front-end/src/features/user/parking-history/ParkingHistory.jsx
@@ -4,6 +4,7 @@ import StatsGrid from "./components/Stats/StatsGrid";
 import HistoryList from "./components/HIstoryList/HistoryList";
 import { useParkingHistory } from "../../../hooks/user/useParkingHistory";
 import { useOutletContext } from "react-router-dom";
+import ParkingHistorySkeleton from "./components/ParkingHIstorySkeleton";
 
 export default function ParkingHistory() {
   const { data, loading, filters, setFilters, handleExport } =
@@ -24,28 +25,34 @@ export default function ParkingHistory() {
     return matchStatus && matchVehicle;
   });
 
-  if (loading) return <div className="text-black">Sedang Loading...</div>;
-
   return (
-    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <Header
-        onExport={handleExport}
-        isExportDisabled={data.history.length === 0}
-      />
-      <Dropdown
-        filters={filters}
-        setFilters={setFilters}
-        vehicleOptions={["Semua Kendaraan", ...userVehicles]}
-      />
-      <StatsGrid stats={data.stats || {}} />
-      <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md py-5 px-6 mt-6">
-        <h2 className="text-[#93A3B6] font-semibold mb-3.5">Daftar Riwayat</h2>
-        {filteredRows.length > 0 ? (
-          <HistoryList rows={filteredRows} />
-        ) : (
-          <p className="text-[#93A3B6]">Belum ada riwayat parkir.</p>
-        )}
-      </div>
-    </section>
+    <>
+      {!loading ? (
+        <ParkingHistorySkeleton />
+      ) : (
+        <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+          <Header
+            onExport={handleExport}
+            isExportDisabled={data.history.length === 0}
+          />
+          <Dropdown
+            filters={filters}
+            setFilters={setFilters}
+            vehicleOptions={["Semua Kendaraan", ...userVehicles]}
+          />
+          <StatsGrid stats={data.stats || {}} />
+          <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md py-5 px-6 mt-6">
+            <h2 className="text-[#93A3B6] font-semibold mb-3.5">
+              Daftar Riwayat
+            </h2>
+            {filteredRows.length > 0 ? (
+              <HistoryList rows={filteredRows} />
+            ) : (
+              <p className="text-[#93A3B6]">Belum ada riwayat parkir.</p>
+            )}
+          </div>
+        </section>
+      )}
+    </>
   );
 }
---
+++ Front-end/src/features/user/parking-history/components/ParkingHistorySkeleton.jsx
@@ -0,0 +1,32 @@
+export default function ParkingHistorySkeleton() {
+  return (
+    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="h-4 w-[20%] bg-[#93A3B6] rounded-md"></h1>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+        </div>
+        <p className="h-3 w-[14%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+      </div>
+      <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md grid grid-cols-2 gap-6 py-5 px-6 mt-6">
+        <div>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+          <p className="h-3 w-full bg-[#93A3B6] rounded-md mt-2.5"></p>
+        </div>
+        <div>
+          <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+          <p className="h-3 w-full bg-[#93A3B6] rounded-md mt-2.5"></p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-6 mt-6">
+        {[...Array(3)].map((_, i) => (
+          <div className="h-20 bg-[#93A3B6] rounded-md" key={i}></div>
+        ))}
+      </div>
+      <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md py-5 px-6 mt-6">
+        <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+        <div className="h-24 bg-[#93A3B6] rounded-md"></div>
+      </div>
+    </section>
+  );
+}
