43d0f57c7acf9863977f15ea62312d50f966d170
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-28 12:21:44 +0700

    chore: add user qrcodeskeleton ui 2.5

--- Front-end/src/features/user/qr-code/components/QRCodeSkeleton.jsx
+++ Front-end/src/features/user/qr-code/components/QRCodeSkeleton.jsx
@@ -2,7 +2,7 @@ export default function QRCodeSkeleton() {
   return (
     <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
       <h1 className="h-4 w-[18%] bg-[#93A3B6] rounded-md"></h1>
-      <p className="h-3 w-[14%] bg-[#93A3B6] rounded-md mt-1"></p>
+      <p className="h-3 w-[14%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-5 flex items-start gap-6">
         <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md p-6 w-[70%]">
           <h2 className="h-4 w-[18%] bg-[#93A3B6] rounded-md mb-3.5"></h2>
@@ -11,8 +11,8 @@ export default function QRCodeSkeleton() {
           <div className="mt-12 border border-[#FFEC78] bg-[#FEF8FD] w-2/5 h-[412px] mx-auto rounded-xl flex items-center justify-center">
             <div className="text-center">
               <div className="w-48 h-48 mb-8 bg-[#93A3B6]"></div>
-              <p className="h-5 w-[14%] bg-[#93A3B6] rounded-md"></p>
-              <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md -mt-1"></p>
+              <p className="h-5 w-[18%] bg-[#93A3B6] mx-auto rounded-md"></p>
+              <p className="h-3 w-[22%] bg-[#93A3B6] mx-auto rounded-md mt-1.5"></p>
             </div>
           </div>
           <div className="grid grid-cols-2 gap-x-6 mt-20">
@@ -21,8 +21,8 @@ export default function QRCodeSkeleton() {
           </div>
         </div>
         <div className="w-[30%]">
+          <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md h-35 mb-2.5"></div>
           <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md h-20"></div>
-          <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md h-14"></div>
         </div>
       </div>
     </section>
