3f07025777f61feccaf23b1ab592f15102be10f7
Rian Priansyah <rian.priansyah04@gmail.com>
2026-03-28 12:15:25 +0700

    chore: add user qrcodeskeleton ui 1.0

--- Front-end/src/features/user/qr-code/QRCode.jsx
+++ Front-end/src/features/user/qr-code/QRCode.jsx
@@ -3,26 +3,31 @@ import VehiclesQR from "./components/VehiclesQR/VehiclesQR";
 import HowToUse from "./components/HowToUse";
 import Information from "./components/Information";
 import { useQrCode } from "../../../hooks/user/useQrCode";
+import QRCodeSkeleton from "./components/QRCodeSkeleton";
 
 export default function QRCode() {
   const { data, loading, handleDownload, handlePrint } = useQrCode();
 
-  if (loading) return <div className="p-5">Loading...</div>;
-
   return (
-    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <Header />
-      <div className="mt-5 flex items-start gap-6">
-        <VehiclesQR
-          data={data}
-          handleDownload={handleDownload}
-          handlePrint={handlePrint}
-        />
-        <div className="w-[30%]">
-          <HowToUse />
-          <Information />
-        </div>
-      </div>
-    </section>
+    <>
+      {loading ? (
+        <QRCodeSkeleton />
+      ) : (
+        <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+          <Header />
+          <div className="mt-5 flex items-start gap-6">
+            <VehiclesQR
+              data={data}
+              handleDownload={handleDownload}
+              handlePrint={handlePrint}
+            />
+            <div className="w-[30%]">
+              <HowToUse />
+              <Information />
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
