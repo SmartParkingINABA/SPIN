e3913170d208387a77e30dc3c7fc6c88b5c24f03
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-03 18:48:45 +0700

    add vehiclesReportSkeleton ui 3.0

--- Front-end/src/features/user/vehicles-report/components/VehiclesReportSkeleton.jsx
+++ Front-end/src/features/user/vehicles-report/components/VehiclesReportSkeleton.jsx
@@ -9,8 +9,8 @@ export default function VehiclesReportSkeleton() {
         <p className="h-6 w-[14%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       </div>
       <div className="grid grid-cols-2 gap-6 mt-7">
-        <div className="h-36 bg-[#93A3B6] rounded-md"></div>
-        <div className="h-36 bg-[#93A3B6] rounded-md"></div>
+        <div className="h-48 bg-[#93A3B6] rounded-md"></div>
+        <div className="h-48 bg-[#93A3B6] rounded-md"></div>
       </div>
     </section>
   );
