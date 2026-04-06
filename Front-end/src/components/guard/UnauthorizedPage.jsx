37a0b32b4300e821572e91e727926e3e48cd047a
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-06 21:41:37 +0700

    update public /unauthorized ui 1.5

--- Front-end/src/components/guard/UnauthorizedPage.jsx
+++ Front-end/src/components/guard/UnauthorizedPage.jsx
@@ -1,12 +1,10 @@
 export default function UnauthorizedPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-[60vh] p-5">
-      <h1 className="font-bold text-[30px] text-red-600">
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
+      <h1 className="font-bold text-3xl uppercase text-[#130F40]">
         403 - Akses Ditolak
       </h1>
-      <p className="text-[#64748b] mt-2">
-        Anda tidak memiliki izin untuk mengakses halaman ini.
-      </p>
+      <p>Anda tidak memiliki izin untuk mengakses halaman ini.</p>
       <button
         onClick={() => (window.location.href = "/")}
         className="mt-5 px-4 py-2 bg-blue-600 text-white rounded-md"
