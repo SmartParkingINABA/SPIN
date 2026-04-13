f868631c5b03cf7cd49436297ed19c14e6e4542f
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-13 16:41:15 +0700

    chore implementasi api endpoint officer scanQRCode 4.0

--- Front-end/src/services/officer/scanQr.Service.js
+++ Front-end/src/services/officer/scanQr.Service.js
@@ -1,9 +1,7 @@
 import api from "../../lib/axios";
 
 export const getScanQr = async (qr_code) => {
-  const response = await api.get("/petugas/dashboard/scan-qr", {
-    params: { qr_code },
-  });
+  const response = await api.post("/petugas/dashboard/scan-qr", { qr_code });
 
   return response.data;
 };
