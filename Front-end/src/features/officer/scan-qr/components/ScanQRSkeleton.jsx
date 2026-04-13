8a5aab09b797a87d24315c75902ae8af6f1b7dc9
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-13 17:18:17 +0700

    chore add ScanQRSkeleton ui 3.0

--- Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
+++ Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
@@ -4,8 +4,8 @@ export default function ScanQRSkeleton() {
       <h1 className="h-4 w-[20%] bg-[#93A3B6] rounded-md"></h1>
       <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-6 grid grid-cols-2 gap-x-5">
-        <div className="h-[484px] bg-[#93A3B6] rounded-md"></div>
-        <div className="h-[484px] bg-[#93A3B6] rounded-md"></div>
+        <div className="h-[584px] bg-[#93A3B6] rounded-md"></div>
+        <div className="h-[584px] bg-[#93A3B6] rounded-md"></div>
       </div>
     </section>
   );
