156a77bf3a04fde64315ae1ce61b7e47261fd6d8
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-13 15:45:39 +0700

    chore implementasi api endpoint officer scanQRCode 1.0

--- Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
+++ Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
@@ -41,11 +41,13 @@ export default function Capture({ isScanning, onScanSuccess }) {
   return (
     <div className="mt-3.5 bg-[#130F40] h-130 flex justify-center items-center rounded-md">
       <div className="text-center border-2 border-[rgba(255,236,120,0.5)] rounded-xl py-13 w-[45%]">
-        <MdOutlineCameraAlt className="h-fit w-13 mx-auto text-[#93A3B6] mb-3" />
         {isScanning ? (
           <div id="qr-reader" className="w-full" ref={qrRef}></div>
         ) : (
-          <p className="text-[#93A3B6]">Kamera siap memindai QR Code</p>
+          <>
+            <MdOutlineCameraAlt className="h-fit w-13 mx-auto text-[#93A3B6] mb-3" />
+            <p className="text-[#93A3B6]">Kamera siap memindai QR Code</p>
+          </>
         )}
       </div>
     </div>
