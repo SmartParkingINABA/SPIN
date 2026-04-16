4d90866b125a5bb16d70af1d8c9db11ec377c3c7
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-16 22:23:15 +0700

    add officer dashboardSkeleton ui 1.0

--- Front-end/src/features/officer/dashboard/components/DashboardSkeleton.jsx
+++ Front-end/src/features/officer/dashboard/components/DashboardSkeleton.jsx
@@ -1,7 +1,7 @@
 export default function DashboardSkeleton() {
   return (
     <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between mb-6">
         <div className="w-[20%]">
           <h1 className="h-4 w-full bg-[#93A3B6] rounded-md"></h1>
           <p className="h-3 w-[80%] bg-[#93A3B6] rounded-md mt-2.5"></p>
