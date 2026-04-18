f3e704aaea188ada99dfb355eba552c5a3d5e985
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-18 13:43:15 +0700

    fix responsive mobile design officer scanQR page 3.0

--- Front-end/src/features/officer/scan-qr/components/Header.jsx
+++ Front-end/src/features/officer/scan-qr/components/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
     <>
-      <h1 className="text-[#FEF8FD] text-2xl font-bold">Scan QR Code</h1>
-      <p className="text-[#93A3B6] font-medium mt-1">
+      <h1 className="text-[#FEF8FD] text-[18px] sm:text-2xl font-bold">
+        Scan QR Code
+      </h1>
+      <p className="text-[#93A3B6] font-medium text-[14px] sm:text-[16px] sm:mt-1">
         Pindai kode QR kendaraan
       </p>
     </>
