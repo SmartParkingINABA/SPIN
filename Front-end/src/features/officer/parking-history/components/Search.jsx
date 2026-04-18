f4c4242aad99ba82fc8fd08e09343aceec63f0a1
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-18 17:11:58 +0700

    fix responsive mobile parkingHistory page

--- Front-end/src/features/officer/parking-history/components/Search.jsx
+++ Front-end/src/features/officer/parking-history/components/Search.jsx
@@ -3,7 +3,7 @@ import { MdDateRange } from "react-icons/md";
 
 export default function Search({ total, search, setSearch }) {
   return (
-    <div className="flex items-center justify-between mb-4.5">
+    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-3.5 sm:mb-4.5">
       <p className="text-[#FEF8FD] font-semibold flex items-center gap-x-2">
         <MdDateRange className="w-5 h-fit" />
         Riwayat Parkir ({total} record)
@@ -15,7 +15,7 @@ export default function Search({ total, search, setSearch }) {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Cari plat nomor atau nama..."
-          className="outline-0 bg-transparent"
+          className="outline-0 bg-transparent placeholder:text-[14px] sm:placeholder:text-[16px]"
         />
       </div>
     </div>
