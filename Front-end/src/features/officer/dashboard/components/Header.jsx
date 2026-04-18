10dae8bb9fdac8a26c02ca56f4e5586302319a81
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-18 13:31:26 +0700

    fix responsive mobile design officer dashboard pagev 2.0

--- Front-end/src/features/officer/dashboard/components/Header.jsx
+++ Front-end/src/features/officer/dashboard/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ user }) {
         <h1 className="text-[#FEF8FD] text-[18px] sm:text-2xl font-bold">
           Dashboard Petugas
         </h1>
-        <p className="text-[#93A3B6] text-[16px] sm:text-[18px] font-medium sm:mt-1">
+        <p className="text-[#93A3B6] text-[14px] sm:text-[18px] font-medium sm:mt-1">
           Selamat datang, <span>{user?.displayName}</span>
         </p>
       </div>
@@ -17,7 +17,7 @@ export default function Header({ user }) {
         className="bg-[#FFDB58] inline-flex items-center gap-x-2 py-2 px-3 rounded-sm transition opacity-100 hover:opacity-80"
       >
         <IoQrCode className="text-[#130F40] w-4 sm:w-5 h-fit" />
-        <p className="text-[#130F40] font-medium text-[16px] sm:text-[18px]">
+        <p className="text-[#130F40] font-medium text-[14px] sm:text-[18px]">
           Mulai Scan
         </p>
       </Link>
