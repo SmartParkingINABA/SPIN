91b1642f19131cb8c52715dc8711c41ad967df00
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-19 12:08:30 +0700

    fix officer mobile profile skeleton 1.0

--- Front-end/src/features/officer/profile/components/ProfileSkeleton.jsx
+++ Front-end/src/features/officer/profile/components/ProfileSkeleton.jsx
@@ -4,11 +4,11 @@ export default function ProfileSkeleton() {
       <h1 className="h-4 w-[40%] sm:w-[20%] bg-[#93A3B6] rounded-md"></h1>
       <p className="h-3 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-4 sm:mt-6 flex flex-col gap-4 sm:gap-6">
-        <div className="flex gap-4 sm:gap-6">
+        <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
           <div className="bg-[#93A3B6] rounded-md w-full sm:w-2/3 h-64"></div>
           <div className="bg-[#93A3B6] rounded-md w-full sm:w-1/3"></div>
         </div>
-        <div className="flex gap-4 sm:gap-6">
+        <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
           <div className="bg-[#93A3B6] rounded-md w-full sm:w-2/3 h-48"></div>
           <div className="bg-[#93A3B6] rounded-md w-full sm:w-1/3"></div>
         </div>
