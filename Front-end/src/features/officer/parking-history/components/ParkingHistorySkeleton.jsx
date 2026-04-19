2fd21e8670920d46df7ff831e170add97dbed7dd
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-19 11:55:32 +0700

    fix officer mobile parkingHistory skeleton

--- Front-end/src/features/officer/parking-history/components/ParkingHistorySkeleton.jsx
+++ Front-end/src/features/officer/parking-history/components/ParkingHistorySkeleton.jsx
@@ -1,9 +1,9 @@
 export default function ParkingHistorySkeleton() {
   return (
     <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
-      <h1 className="h-4 w-[20%] bg-[#93A3B6] rounded-md"></h1>
-      <p className="h-3 w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
-      <div className="mt-6 h-64 bg-[#93A3B6] rounded-md"></div>
+      <h1 className="h-4 w-[40%] sm:w-[20%] bg-[#93A3B6] rounded-md"></h1>
+      <p className="h-3 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
+      <div className="mt-4 sm:mt-6 h-64 bg-[#93A3B6] rounded-md"></div>
     </section>
   );
 }
