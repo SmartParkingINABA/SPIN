9f5a8b0026ed1dea80084e559329f731f9178192
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-19 11:48:20 +0700

    fix officer mobile scanQR skeleton 1.5

--- Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
+++ Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
@@ -4,8 +4,8 @@ export default function ScanQRSkeleton() {
       <h1 className="h-4 w-[40%] sm:w-[20%] bg-[#93A3B6] rounded-md"></h1>
       <p className="h-3 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-4 sm:mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
-        <div className="h-[584px] bg-[#93A3B6] rounded-md"></div>
-        <div className="h-[584px] bg-[#93A3B6] rounded-md"></div>
+        <div className="h-[300px] sm:h-[584px] bg-[#93A3B6] rounded-md"></div>
+        <div className="h-[300px] sm:h-[584px] bg-[#93A3B6] rounded-md"></div>
       </div>
     </section>
   );
