5d63ca2ce471641410a540d9e73b7315efffc69d
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 14:39:36 +0700

    fix responsive mobile parking history page

--- Front-end/src/features/user/parking-history/components/Stats/StatsCard.jsx
+++ Front-end/src/features/user/parking-history/components/Stats/StatsCard.jsx
@@ -2,13 +2,13 @@ export default function StatsCard({ title, value, icon }) {
   const Icon = icon;
 
   return (
-    <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] flex items-center gap-x-4.5 rounded-md py-5 px-6">
-      <div className="p-3 bg-[#4B4141] rounded-sm">
-        <Icon className="h-fit w-6 text-[#FFEC78]" />
+    <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] flex items-center gap-x-4 sm:gap-x-4.5 rounded-md p-5 sm:p-6">
+      <div className="p-2.5 sm:p-3 bg-[#4B4141] rounded-sm">
+        <Icon className="h-fit w-5 sm:w-6 text-[#FFEC78]" />
       </div>
-      <div className="text-center">
-        <p className="text-[#93A3B6]">{title}</p>
-        <p className="text-[#FEF8FD]">{value}</p>
+      <div className="text-start">
+        <p className="text-[#93A3B6] text-[14px] sm:text-[16px]">{title}</p>
+        <p className="text-[#FEF8FD] text-[14px] sm:text-[16px]">{value}</p>
       </div>
     </div>
   );
