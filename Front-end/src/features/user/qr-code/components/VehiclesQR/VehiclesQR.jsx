2c2fcaebd4ae8bc792aa96b09317a4dad9144a91
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 12:18:53 +0700

    fix responsive mobile user QR Code page 1.0

--- Front-end/src/features/user/qr-code/components/VehiclesQR/VehiclesQR.jsx
+++ Front-end/src/features/user/qr-code/components/VehiclesQR/VehiclesQR.jsx
@@ -11,7 +11,7 @@ export default function VehiclesQR({
 }) {
   return (
     <div className="bg-[#1E1633] border border-[rgba(255,236,120,0.5)] rounded-md p-6 w-full sm:w-[70%]">
-      <h2 className="text-[#FEF8FD] text-[18px] font-semibold mb-3.5">
+      <h2 className="text-[#FEF8FD] text-[16px] sm:text-[18px] font-semibold mb-3 sm:mb-3.5">
         QR Code Kendaraan
       </h2>
       <p className="text-[#93A3B6] font-medium mb-2.5">Pilih Kendaraan</p>
