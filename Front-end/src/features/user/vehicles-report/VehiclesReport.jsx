55dbca5ef7fdb4c7bc16de4cd210d41ef45cfc2c
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 11:51:44 +0700

    fix responsive mobile user vehicle report page 2.5

--- Front-end/src/features/user/vehicles-report/VehiclesReport.jsx
+++ Front-end/src/features/user/vehicles-report/VehiclesReport.jsx
@@ -16,7 +16,7 @@ export default function VehiclesReport() {
     <>
       <section className="bg-[#130F40] px-5 py-7 h-[calc(100dvh-60px)] overflow-y-auto">
         <Header setIsModalOpen={setIsModalOpen} />
-        <div className="grid grid-cols-2 gap-6 mt-7">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 mt-4 sm:mt-6">
           {vehicles.length > 0 ? (
             <VehicleGrid
               vehicles={vehicles}
