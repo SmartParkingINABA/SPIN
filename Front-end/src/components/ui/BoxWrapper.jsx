e1f1b82e75e6f34328b3d63723149bcb85efade5
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 12:52:30 +0700

    fix responsive mobile user QR Code page 6.0

--- Front-end/src/components/ui/BoxWrapper.jsx
+++ Front-end/src/components/ui/BoxWrapper.jsx
@@ -1,6 +1,6 @@
 export default function BoxWrapper({ title, children }) {
   return (
-    <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] px-5 sm:px-6 py-4 rounded-md">
+    <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] p-5 sm:p-6 rounded-md">
       <p className="text-[#FEF8FD] font-medium">{title}</p>
       {children}
     </div>
