62ffc4d6c517cc399f81d6e1dc3777ad2c7fe37e
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 14:14:42 +0700

    fix responsive mobile parking history page

--- Front-end/src/features/user/parking-history/components/Header.jsx
+++ Front-end/src/features/user/parking-history/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header({ onExport, isExportDisabled }) {
         <h1 className="text-[#FEF8FD] text-[18px] sm:text-2xl font-bold">
           Riwayat Parkir
         </h1>
-        <p className="text-[#93A3B6]text-[14px] sm:text-[16px] font-medium sm:mt-1">
-          Catatan aktivitas parkir kendaraan anda
+        <p className="text-[#93A3B6] text-[14px] sm:text-[16px] font-medium sm:mt-1">
+          Catatan parkir kendaraan anda
         </p>
       </div>
       <ExportData onExport={onExport} isDisabled={isExportDisabled} />
