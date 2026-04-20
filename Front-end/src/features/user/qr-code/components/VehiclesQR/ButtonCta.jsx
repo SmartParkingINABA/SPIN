b6bd8804a8d62b9e5afda88e8217d30f38056259
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 12:30:00 +0700

    fix responsive mobile user QR Code page 3.0

--- Front-end/src/features/user/qr-code/components/VehiclesQR/ButtonCta.jsx
+++ Front-end/src/features/user/qr-code/components/VehiclesQR/ButtonCta.jsx
@@ -4,7 +4,7 @@ export default function ButtonCta({ onDownload, onPrint }) {
   return (
     <div className="grid grid-cols-2 gap-4 sm:gap-6 mt-16 sm:mt-20">
       <button
-        className="bg-[#FFDB58] rounded-md border-0 py-1 sm:py-2 transition cursor-pointer opacity-100 hover:opacity-80"
+        className="bg-[#FFDB58] rounded-md border-0 py-2 transition cursor-pointer opacity-100 hover:opacity-80"
         onClick={onDownload}
       >
         <div className="flex items-center gap-1.5 sm:gap-2.5 mx-auto w-fit">
@@ -15,7 +15,7 @@ export default function ButtonCta({ onDownload, onPrint }) {
         </div>
       </button>
       <button
-        className="bg-[#FFDB58] rounded-md border-0 py-1 sm:py-2 transition cursor-pointer opacity-100 hover:opacity-80"
+        className="bg-[#FFDB58] rounded-md border-0 py-2 transition cursor-pointer opacity-100 hover:opacity-80"
         onClick={onPrint}
       >
         <div className="flex items-center gap-1.5 sm:gap-2.5 mx-auto w-fit">
