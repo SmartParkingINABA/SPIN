5c5cb9642769ebb1c8771fbb70b4aa543aaf6135
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-20 13:21:26 +0700

    fix responsive mobile dashboard page 3.0

--- Front-end/src/features/user/dashboard/components/Stats/StatsCard.jsx
+++ Front-end/src/features/user/dashboard/components/Stats/StatsCard.jsx
@@ -4,7 +4,7 @@ export default function StatsCard({ title, value, icon }) {
     <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] flex items-center justify-between p-5 sm:p-6 rounded-md">
       <div className="text-start">
         <p className="text-[#93A3B6] text-[14px] sm:text-[16px]">{title}</p>
-        <p className="text-[#FEF8FD]">{value}</p>
+        <p className="text-[#FEF8FD] text-[14px] sm:text-[16px]">{value}</p>
       </div>
       <div className="bg-[#4B4141] rounded-full p-2.5 sm:p-3 flex justify-center items-center">
         <Icon className="text-[#FFEC78] w-5 sm:w-6 h-fit" />
