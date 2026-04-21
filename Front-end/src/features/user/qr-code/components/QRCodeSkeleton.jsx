272a306d7ae223327ee5c6425a548ca5e7feef43
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-21 12:44:57 +0700

    fix responsive mobile user QR Code skeleton page

--- Front-end/src/features/user/qr-code/components/QRCodeSkeleton.jsx
+++ Front-end/src/features/user/qr-code/components/QRCodeSkeleton.jsx
@@ -8,16 +8,16 @@ export default function QRCodeSkeleton() {
           <h2 className="h-4 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md mb-1.5 sm:mb-2.5"></h2>
           <p className="h-3 w-[36%] sm:w-[16%] bg-[#93A3B6] rounded-md mb-2.5 sm:mb-3.5"></p>
           <p className="h-8 w-full bg-[#93A3B6] rounded-md"></p>
-          <div className="mt-10 sm:mt-12 border border-[#FFEC78] bg-[#FEF8FD] w-full sm:w-2/5 h-[412px] mx-auto rounded-xl flex items-center justify-center">
+          <div className="mt-8 sm:mt-12 border border-[#FFEC78] bg-[#FEF8FD] w-full sm:w-2/5 h-[412px] mx-auto rounded-xl flex items-center justify-center">
             <div className="text-center">
               <div className="w-48 h-48 mb-8 bg-[#93A3B6]"></div>
               <p className="h-4 w-[25%] bg-[#93A3B6] mx-auto rounded-md"></p>
               <p className="h-3.5 w-[50%] bg-[#93A3B6] mx-auto rounded-md mt-1.5"></p>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-4 sm:gap-6 mt-18 sm:mt-20">
-            <p className="h-6 bg-[#93A3B6] rounded-md"></p>
-            <p className="h-6 bg-[#93A3B6] rounded-md"></p>
+          <div className="grid grid-cols-2 gap-4 sm:gap-6 mt-16 sm:mt-20">
+            <p className="h-8 bg-[#93A3B6] rounded-md"></p>
+            <p className="h-8 bg-[#93A3B6] rounded-md"></p>
           </div>
         </div>
         <div className="w-full sm:w-[30%]">
