11cc7c4529a8092e072b6436b95720b8a2bb1f53
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-21 14:09:14 +0700

    fix responsive mobile user profile page

--- Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
+++ Front-end/src/features/user/profile/components/ProfileSkeleton.jsx
@@ -13,20 +13,20 @@ export default function ProfileSkeleton() {
               </div>
               <form className="mt-2.5 sm:mt-3.5 w-full sm:w-1/2" noValidate>
                 <div className="mb-1.5 sm:mb-2.5">
-                  <p className="h-3 w-[22%] bg-[#93A3B6] rounded-md"></p>
-                  <p className="h-3 w-[18%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
+                  <p className="h-3 w-[35%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[30%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
                 </div>
                 <div className="mb-1.5 sm:mb-2.5">
-                  <p className="h-3 w-[22%] bg-[#93A3B6] rounded-md"></p>
-                  <p className="h-3 w-[18%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
+                  <p className="h-3 w-[35%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[30%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
                 </div>
                 <div className="mb-1.5 sm:mb-2.5">
-                  <p className="h-3 w-[22%] bg-[#93A3B6] rounded-md"></p>
-                  <p className="h-3 w-[18%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
+                  <p className="h-3 w-[35%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[30%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
                 </div>
                 <div className="">
-                  <p className="h-3 w-[22%] bg-[#93A3B6] rounded-md"></p>
-                  <p className="h-3 w-[18%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
+                  <p className="h-3 w-[35%] bg-[#93A3B6] rounded-md"></p>
+                  <p className="h-3 w-[30%] bg-[#130F40] rounded-md mt-1.5 sm:mt-2.5"></p>
                 </div>
               </form>
             </div>
@@ -34,24 +34,24 @@ export default function ProfileSkeleton() {
               <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] p-5 sm:p-6 rounded-md">
                 <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md mb-4 sm:mb-6"></p>
                 <div className="w-30 h-30 mx-auto rounded-full bg-[#93A3B6]"></div>
-                <p className="h-3 w-[13%] bg-[#93A3B6] rounded-md mx-auto mt-4 mb-2"></p>
-                <p className="h-3 w-[18%] bg-[#130F40] rounded-md mx-auto"></p>
+                <p className="h-3 w-[30%] bg-[#93A3B6] rounded-md mx-auto mt-4 mb-2"></p>
+                <p className="h-3 w-[35%] bg-[#130F40] rounded-md mx-auto"></p>
                 <p className="bg-[#93A3B6] rounded-sm py-1.5 h-5 w-full mt-3.5"></p>
               </div>
               <div className="border border-[rgba(255,236,120,0.5)] bg-[#1E1633] p-5 sm:p-6 rounded-md">
                 <p className="h-3 w-[15%] bg-[#93A3B6] rounded-md"></p>
                 <div className="mt-2.5 sm:mt-3.5 flex flex-col gap-y-2.5 sm:gap-y-3.5">
                   <div className="pb-3 border-b border-b-[rgba(255,236,120,0.5)] flex flex-col gap-2.5">
-                    <p className="h-3 w-[15%] bg-[#130F40] rounded-md"></p>
-                    <p className="h-3.5 w-[18%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3 w-[30%] bg-[#130F40] rounded-md"></p>
+                    <p className="h-3.5 w-[35%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                   <div className="pb-3 border-b border-b-[rgba(255,236,120,0.5)] flex flex-col gap-2.5">
-                    <p className="h-3 w-[15%] bg-[#130F40] rounded-md"></p>
-                    <p className="h-3.5 w-[18%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3 w-[30%] bg-[#130F40] rounded-md"></p>
+                    <p className="h-3.5 w-[35%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                   <div className="flex flex-col gap-2.5">
-                    <p className="h-3 w-[15%] bg-[#130F40] rounded-md"></p>
-                    <p className="h-3.5 w-[18%] bg-[#93A3B6] rounded-md"></p>
+                    <p className="h-3 w-[30%] bg-[#130F40] rounded-md"></p>
+                    <p className="h-3.5 w-[35%] bg-[#93A3B6] rounded-md"></p>
                   </div>
                 </div>
               </div>
