4d39d6303fae1c015b927d2625952ceb36d65f24
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-21 13:11:02 +0700

    fix responsive mobile user notification page

--- Front-end/src/features/user/notification/components/NotificationSkeleton.jsx
+++ Front-end/src/features/user/notification/components/NotificationSkeleton.jsx
@@ -1,6 +1,6 @@
 export default function NotificationSkeleton() {
   return (
-    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+    <section className="bg-[#130F40] px-5 py-7 h-[calc(100dvh-60px)] overflow-y-auto">
       <h1 className="h-4 w-[40%] sm:w-[20%] bg-[#93A3B6] rounded-md"></h1>
       <div className="mt-1.5 flex items-center justify-between">
         <p className="h-3 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md"></p>
