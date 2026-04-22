c14c8b4bd88afe79c72263a9c9f255d3682577da
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-22 11:46:52 +0700

    fix h-screen officer skeleton page

--- Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
+++ Front-end/src/features/officer/scan-qr/components/ScanQRSkeleton.jsx
@@ -1,6 +1,6 @@
 export default function ScanQRSkeleton() {
   return (
-    <section className="bg-[#130F40] px-5 py-7 h-[calc(100vh-60px)] overflow-y-auto">
+    <section className="bg-[#130F40] px-5 py-7 h-[calc(100dvh-60px)] overflow-y-auto">
       <h1 className="h-4 w-[40%] sm:w-[20%] bg-[#93A3B6] rounded-md"></h1>
       <p className="h-3 w-[38%] sm:w-[18%] bg-[#93A3B6] rounded-md mt-2.5"></p>
       <div className="mt-4 sm:mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
