992c7b829d588a7cd6f0fce37bf62b930f56a0c1
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-22 12:00:45 +0700

    fix officer ScanQr page

--- Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
+++ Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
@@ -40,7 +40,7 @@ export default function Capture({ isScanning, onScanSuccess }) {
 
   return (
     <div className="mt-3.5 bg-[#130F40] h-100 sm:h-130 flex justify-center items-center rounded-md p-5 sm:p-0">
-      <div className="text-center border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full sm:w-[50%]">
+      <div className="text-center border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full py-10 sm:py-12 sm:w-[50%]">
         {isScanning ? (
           <div id="qr-reader" className="w-full" ref={qrRef}></div>
         ) : (
