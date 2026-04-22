f5caf00ea1a871e11e17d1dea2924b822ba28b72
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-22 13:05:07 +0700

    fix officer ScanQr page

--- Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
+++ Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
@@ -40,11 +40,11 @@ export default function Capture({ isScanning, onScanSuccess }) {
 
   return (
     <div className="mt-3.5 bg-[#130F40] h-100 sm:h-130 flex justify-center items-center rounded-md p-6 sm:p-0">
-      <div className="border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full h-fit overflow-hidden flex items-center justify-center">
+      <div className="border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full h-[320px] overflow-hidden flex items-center justify-center">
         {isScanning ? (
-          <div id="qr-reader" className="w-full" ref={qrRef}></div>
+          <div id="qr-reader" className="w-full h-full" ref={qrRef}></div>
         ) : (
-          <div className="flex flex-col justify-center items-center h-[330px]">
+          <div className="flex flex-col justify-center items-center h-full">
             <MdOutlineCameraAlt className="h-fit w-13 mx-auto text-[#93A3B6] mb-2 sm:mb-3" />
             <p className="text-[#93A3B6] text-center">
               Kamera siap memindai QR Code
