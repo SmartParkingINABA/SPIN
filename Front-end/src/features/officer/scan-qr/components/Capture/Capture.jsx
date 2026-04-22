72e45dea6a0c14e858a476cdd56a259daf169679
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-22 12:15:05 +0700

    fix officer ScanQr page

--- Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
+++ Front-end/src/features/officer/scan-qr/components/Capture/Capture.jsx
@@ -39,15 +39,15 @@ export default function Capture({ isScanning, onScanSuccess }) {
   }, [isScanning, onScanSuccess]);
 
   return (
-    <div className="mt-3.5 bg-[#130F40] h-100 sm:h-130 flex justify-center items-center rounded-md p-5 sm:p-0">
-      <div className="text-center border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full h-full overflow-hidden flex items-center justify-center sm:h-fit sm:py-13 sm:w-[50%]">
+    <div className="mt-3.5 bg-[#130F40] h-100 sm:h-130 flex justify-center items-center rounded-md p-7.5 sm:p-0">
+      <div className="border-2 border-[rgba(255,236,120,0.5)] rounded-xl w-full h-full overflow-hidden flex items-center justify-center sm:h-fit sm:py-13 sm:w-[50%]">
         {isScanning ? (
-          <div id="qr-reader" className="w-full h-full" ref={qrRef}></div>
+          <div id="qr-reader" className="w-full" ref={qrRef}></div>
         ) : (
-          <>
+          <div className="flex flex-col items-center">
             <MdOutlineCameraAlt className="h-fit w-13 mx-auto text-[#93A3B6] mb-3" />
             <p className="text-[#93A3B6]">Kamera siap memindai QR Code</p>
-          </>
+          </div>
         )}
       </div>
     </div>
