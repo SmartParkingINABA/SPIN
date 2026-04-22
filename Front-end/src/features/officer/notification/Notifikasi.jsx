9fdde4a68d254f19b9d3cdc3e24d622c82ac6468
Rian Priansyah <rian.priansyah04@gmail.com>
2026-04-22 20:55:17 +0700

    fix responsive mobile petugas notification

--- Front-end/src/features/officer/notification/Notifikasi.jsx
+++ Front-end/src/features/officer/notification/Notifikasi.jsx
@@ -20,7 +20,7 @@ export default function Notifikasi() {
   return (
     <section className="bg-[#130F40] px-5 py-7 h-[calc(100dvh-60px)] overflow-y-auto">
       <Header unreadCount={unreadCount} onReadAll={readAllNotification} />
-      <div className="mt-6 flex flex-col gap-y-3.5">
+      <div className="mt-4 sm:mt-6 flex flex-col gap-y-2.5 sm:gap-y-3.5">
         {notifications.length > 0 ? (
           notifications.map((notif) => (
             <NotificationCard
@@ -30,7 +30,7 @@ export default function Notifikasi() {
             />
           ))
         ) : (
-          <p className="text-[#93A3B6]">Belum ada notifikasi.</p>
+          <p className="text-[#93A3B6] text-[14px]">Belum ada notifikasi.</p>
         )}
       </div>
       <Pagination pagination={pagination} setPagination={setPagination} />
--- Front-end/src/features/officer/notification/components/Header.jsx
+++ Front-end/src/features/officer/notification/components/Header.jsx
@@ -6,21 +6,21 @@ export default function Header({ unreadCount, onReadAll }) {
       <h1 className="text-[#FEF8FD] text-[18px] sm:text-2xl font-bold flex items-center gap-x-2">
         Notifikasi Sistem
         {unreadCount > 0 && (
-          <span className="text-[#FEF8FD] text-[10px] sm:text-[12px] bg-red-500 rounded-sm px-1 py-0.5">
+          <span className="text-[#FEF8FD] text-[12px] bg-red-500 rounded-sm px-1 py-0.5">
             {unreadCount} Baru
           </span>
         )}
       </h1>
-      <div className="mt-1 flex items-center justify-between">
-        <p className="text-[#93A3B6] font-medium">
+      <div className="sm:mt-1 flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <p className="text-[#93A3B6] text-[14px] sm:text-[16px] font-medium mb-1 sm:mb-0">
           Peringatan dan pesan penting dari sistem
         </p>
         {unreadCount > 0 && (
           <button
-            className="flex items-center gap-x-2 border border-transparent bg-[#93A3B6] py-1 px-1.5 rounded-sm cursor-pointer hover:opacity-80"
+            className="flex items-center gap-x-2 border border-transparent bg-[#93A3B6] py-1 px-1.5 rounded-sm cursor-pointer hover:opacity-80 mt-1.5 sm:mt-0"
             onClick={onReadAll}
           >
-            <BiCheckDouble className="h-fit w-5 text-[#130F40]" />
+            <BiCheckDouble className="h-fit w-4 sm:w-5 text-[#130F40]" />
             <p className="text-[#130F40] text-[14px]">
               Tandai Semua Telah Dibaca
             </p>
